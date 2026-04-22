c66619782d8b16624f0b10485b001a0528431383
topg85 <danielcarson1985@gmail.com>
2026-04-22 19:59:55 +0100

    feat: Add three resource pages for Alpha, CAP, and Hope Central
    - Create Wagtail page models:
      * AlphaCoursePage: Alpha course info, YouTube videos, local sessions, sign-up
      * CAPPage: CAP overview, services, impact stats, YouTube content, local referrals
      * HopeCentralPage: Mission, services, 2024 impact stats, volunteering, contact
    
    - Add corresponding Django template files:
      * alpha_course_page.html: Sections for overview, what's covered, why join, videos, local info
      * cap_page.html: About CAP, services, impact, YouTube, local services, help section
      * hope_central_page.html: Mission, services, impact stats grid, get involved, contact
    
    - Create database migration for three new models
    - Update navigation menu in index.html to include Alpha, CAP, Hope Central links
    - Update Resources section cards to link to new pages instead of external sites
    - Pages editable in Wagtail admin at /admin/pages/
    
    Access the pages at:
      - /alpha/ - Alpha course information and sign-up
      - /cap/ - Christians Against Poverty services and support
      - /hope-central/ - Hope Central mission, services, and volunteering info

--- core/models.py
+++ core/models.py
@@ -224,3 +224,229 @@ class HomePage(Page):
     ]
 
 
+class AlphaCoursePage(Page):
+    """Alpha Course page with overview, what's covered, and sign-up info"""
+    
+    # Overview Section
+    overview_title = models.CharField(max_length=255, blank=True, default="What is Alpha?")
+    overview_content = RichTextField(blank=True, help_text="Description of Alpha course")
+    overview_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Alpha course overview image"
+    )
+    
+    # What's Covered Section
+    covered_title = models.CharField(max_length=255, blank=True, default="What's Covered")
+    covered_content = RichTextField(blank=True, help_text="Topics and sessions covered in the course")
+    
+    # Why Join Section
+    why_join_title = models.CharField(max_length=255, blank=True, default="Why Join Alpha?")
+    why_join_content = RichTextField(blank=True, help_text="Benefits and reasons to join")
+    
+    # YouTube Content
+    youtube_title = models.CharField(max_length=255, blank=True, default="Alpha Videos")
+    youtube_embed_1 = models.URLField(blank=True, help_text="YouTube embed URL 1")
+    youtube_embed_2 = models.URLField(blank=True, help_text="YouTube embed URL 2")
+    youtube_playlist_url = models.URLField(blank=True, help_text="Alpha YouTube playlist URL")
+    
+    # Local Information
+    local_title = models.CharField(max_length=255, blank=True, default="Alpha at Vinelife Wilmslow")
+    local_info = RichTextField(blank=True, help_text="Local session times and contact info")
+    
+    # Sign-Up Section
+    signup_title = models.CharField(max_length=255, blank=True, default="Sign Up Today")
+    signup_intro = models.CharField(max_length=255, blank=True, default="Ready to explore faith?")
+    alpha_website_url = models.URLField(blank=True, default="https://alpha.org.uk/try-alpha")
+    
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('overview_title'),
+            FieldPanel('overview_image'),
+            FieldPanel('overview_content'),
+        ], heading='Overview'),
+        
+        MultiFieldPanel([
+            FieldPanel('covered_title'),
+            FieldPanel('covered_content'),
+        ], heading='What\'s Covered'),
+        
+        MultiFieldPanel([
+            FieldPanel('why_join_title'),
+            FieldPanel('why_join_content'),
+        ], heading='Why Join'),
+        
+        MultiFieldPanel([
+            FieldPanel('youtube_title'),
+            FieldPanel('youtube_embed_1'),
+            FieldPanel('youtube_embed_2'),
+            FieldPanel('youtube_playlist_url'),
+        ], heading='YouTube Content'),
+        
+        MultiFieldPanel([
+            FieldPanel('local_title'),
+            FieldPanel('local_info'),
+        ], heading='Local Information'),
+        
+        MultiFieldPanel([
+            FieldPanel('signup_title'),
+            FieldPanel('signup_intro'),
+            FieldPanel('alpha_website_url'),
+        ], heading='Sign Up'),
+    ]
+
+
+class CAPPage(Page):
+    """Christians Against Poverty (CAP) page with services and support info"""
+    
+    # What is CAP Section
+    about_title = models.CharField(max_length=255, blank=True, default="What is CAP?")
+    about_content = RichTextField(blank=True, help_text="Introduction to CAP")
+    about_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="CAP logo or image"
+    )
+    
+    # Services Section
+    services_title = models.CharField(max_length=255, blank=True, default="Our Services")
+    services_content = RichTextField(blank=True, help_text="Details about CAP services: debt help, money coaching, job clubs, life skills")
+    
+    # Impact Section
+    impact_title = models.CharField(max_length=255, blank=True, default="Our Impact")
+    impact_content = RichTextField(blank=True, help_text="Impact stories and statistics")
+    people_helped = models.CharField(max_length=100, blank=True, default="35,000+", help_text="Number of people helped")
+    
+    # YouTube Content
+    youtube_title = models.CharField(max_length=255, blank=True, default="CAP Videos")
+    youtube_embed_1 = models.URLField(blank=True, help_text="YouTube embed URL 1")
+    youtube_embed_2 = models.URLField(blank=True, help_text="YouTube embed URL 2")
+    youtube_channel_url = models.URLField(blank=True, help_text="CAP YouTube channel URL")
+    
+    # Local Services
+    local_title = models.CharField(max_length=255, blank=True, default="CAP at Vinelife Wilmslow")
+    local_services = RichTextField(blank=True, help_text="Services available at Vinelife/Hope Central")
+    
+    # Get Help Section
+    help_title = models.CharField(max_length=255, blank=True, default="Get Free Help")
+    help_intro = RichTextField(blank=True, help_text="How to access CAP support")
+    cap_website_url = models.URLField(blank=True, default="https://www.capuk.org/get-help/debt-help")
+    local_contact = models.CharField(max_length=255, blank=True, help_text="Local contact info for referrals")
+    
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('about_title'),
+            FieldPanel('about_image'),
+            FieldPanel('about_content'),
+        ], heading='About CAP'),
+        
+        MultiFieldPanel([
+            FieldPanel('services_title'),
+            FieldPanel('services_content'),
+        ], heading='Services'),
+        
+        MultiFieldPanel([
+            FieldPanel('impact_title'),
+            FieldPanel('people_helped'),
+            FieldPanel('impact_content'),
+        ], heading='Impact'),
+        
+        MultiFieldPanel([
+            FieldPanel('youtube_title'),
+            FieldPanel('youtube_embed_1'),
+            FieldPanel('youtube_embed_2'),
+            FieldPanel('youtube_channel_url'),
+        ], heading='YouTube Content'),
+        
+        MultiFieldPanel([
+            FieldPanel('local_title'),
+            FieldPanel('local_services'),
+        ], heading='Local Services'),
+        
+        MultiFieldPanel([
+            FieldPanel('help_title'),
+            FieldPanel('help_intro'),
+            FieldPanel('local_contact'),
+            FieldPanel('cap_website_url'),
+        ], heading='Get Help'),
+    ]
+
+
+class HopeCentralPage(Page):
+    """Hope Central page with services, impact, and involvement info"""
+    
+    # Mission Section
+    mission_title = models.CharField(max_length=255, blank=True, default="About Hope Central")
+    mission_content = RichTextField(blank=True, help_text="Hope Central mission and overview")
+    mission_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Hope Central logo or image"
+    )
+    
+    # Services Section
+    services_title = models.CharField(max_length=255, blank=True, default="Services We Offer")
+    services_content = RichTextField(blank=True, help_text="Food bank, pantry, job club, debt centre, money management, Alpha, Hope Centres")
+    
+    # Impact Section
+    impact_title = models.CharField(max_length=255, blank=True, default="Our Impact")
+    referrals_2024 = models.CharField(max_length=100, blank=True, default="3,400+", help_text="Number of referrals in 2024")
+    meals_2024 = models.CharField(max_length=100, blank=True, default="65,500", help_text="Number of meals provided in 2024")
+    impact_content = RichTextField(blank=True, help_text="Impact stats and stories")
+    
+    # Get Involved Section
+    involved_title = models.CharField(max_length=255, blank=True, default="Get Involved")
+    volunteer_content = RichTextField(blank=True, help_text="Volunteering opportunities")
+    donate_url = models.URLField(blank=True, default="https://cafdonate.cafonline.org/17913")
+    
+    # Contact & Resources
+    contact_title = models.CharField(max_length=255, blank=True, default="Contact Us")
+    contact_info = RichTextField(blank=True, help_text="Contact information and hours")
+    phone = models.CharField(max_length=20, blank=True, default="01625 724 133")
+    email = models.EmailField(blank=True, default="info@hopecentral.org.uk")
+    website_url = models.URLField(blank=True, default="https://www.hopecentral.org.uk")
+    
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('mission_title'),
+            FieldPanel('mission_image'),
+            FieldPanel('mission_content'),
+        ], heading='Mission'),
+        
+        MultiFieldPanel([
+            FieldPanel('services_title'),
+            FieldPanel('services_content'),
+        ], heading='Services'),
+        
+        MultiFieldPanel([
+            FieldPanel('impact_title'),
+            FieldPanel('referrals_2024'),
+            FieldPanel('meals_2024'),
+            FieldPanel('impact_content'),
+        ], heading='Impact'),
+        
+        MultiFieldPanel([
+            FieldPanel('involved_title'),
+            FieldPanel('volunteer_content'),
+            FieldPanel('donate_url'),
+        ], heading='Get Involved'),
+        
+        MultiFieldPanel([
+            FieldPanel('contact_title'),
+            FieldPanel('phone'),
+            FieldPanel('email'),
+            FieldPanel('contact_info'),
+            FieldPanel('website_url'),
+        ], heading='Contact Information'),
+    ]
+
+
